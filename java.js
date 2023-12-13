380c71d21c292a949a4c310f7e0e3f3cd78dac92
Shekah-Abdalaziz20 <153465980+Shekah-Abdalaziz20@users.noreply.github.com>
2023-12-13 23:17:56 +0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,28 @@
+ function link(){
+	    window.open("file:///C:/Users/LENOVO/Documents/level%206/project-web/sweets.html")
+		
+		
+}
+function cc(){
+	    window.open("file:///C:/Users/LENOVO/Documents/level%206/project-web/project_web.html#")
+		
+		
+}
+function ss(){
+	    window.open("file:///C:/Users/LENOVO/Documents/level%206/project-web/Appetisers%20and%20Salads.html")
+		
+		
+}
+function kk(){
+	    window.open("file:///C:/Users/LENOVO/Documents/level%206/project-web/Main%20Dishes.html")
+		
+		
+}
+function hh(){
+	    window.open("file:///C:/Users/LENOVO/Documents/level%206/project-web/home.html")
+		
+		
+}
+
+
+
